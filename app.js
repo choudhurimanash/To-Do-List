0d2523d005940711d2ba6c0107c176736bedbaaa
MANASH CHOUDHURI <manashchoudhuri4@gmail.com>
2024-02-29 22:18:29 +0530

    This is an To Do list app

---
+++ app.js
@@ -0,0 +1,42 @@
+const inputBox = document.querySelector("#input-box");
+// const btn = document.querySelector("button");
+const listContainer = document.querySelector("#list-container");
+
+const addTask = () => {
+  if (inputBox.value === "") {
+    alert("You write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+};
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showTask = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showTask();
